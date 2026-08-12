283f1fc0b6b7338ee4185af29b3df4bfcf2fee75
Kaushik Brahmbhatt <kaushik.brahmbhatt@mcs.com>
2026-08-12 14:20:11 +0530

    Update default API URL to local IP http://192.168.1.49:8000/api

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,7 +1,7 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ApiConfig {
-  static const String defaultBaseUrl = 'https://ominous-dollop-xqj95p9x4q6cv76j-8000.app.github.dev/api';
+  static const String defaultBaseUrl = 'http://192.168.1.49:8000/api';
   
   static Future<String> getBaseUrl() async {
     final prefs = await SharedPreferences.getInstance();
