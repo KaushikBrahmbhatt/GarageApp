4e44b09e4abf34703bf0760340ef2431a0096a53
Kaushik Brahmbhatt <kaushik.brahmbhatt@mcs.com>
2026-08-12 14:48:00 +0530

    Fix Android INTERNET permissions and add HttpOverrides for tunnel network calls

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -7,8 +8,18 @@ import 'providers/auth_provider.dart';
 import 'providers/dashboard_provider.dart';
 import 'providers/job_card_provider.dart';
 
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+  }
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  HttpOverrides.global = MyHttpOverrides();
+  
   final prefs = await SharedPreferences.getInstance();
   final token = prefs.getString('token');
   
